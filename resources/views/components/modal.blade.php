7ead46925e64d68537dfaaac7985c3a18e267974
Ryan <vellabonelloryan@gmail.com>
2023-12-05 12:50:57 +0800

    adds smooth transitions between show/hide states

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -9,31 +9,32 @@
     class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true"
     style="display: none"
 >
-    <!--
-        Background backdrop, show/hide based on modal state.
-
-        Entering: "ease-out duration-300"
-            From: "opacity-0"
-            To: "opacity-100"
-        Leaving: "ease-in duration-200"
-            From: "opacity-100"
-            To: "opacity-0"
-    -->
-    <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+    <!-- Background backdrop, show/hide based on modal state. -->
+    <div
+        class="fixed inset-0 bg-gray-500 bg-opacity-75"
+        x-show="open"
+        x-transition:enter="ease-out duration-300"
+        x-transition:enter-start="opacity-0"
+        x-transition:enter-end="opacity-100"
+        x-transition:leave="ease-in duration-200"
+        x-transition:leave-start="opacity-100"
+        x-transition:leave-end="opacity-0"
+    ></div>
 
     <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
         <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-            <!--
-                Modal panel, show/hide based on modal state.
-
-                Entering: "ease-out duration-300"
-                    From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                    To: "opacity-100 translate-y-0 sm:scale-100"
-                Leaving: "ease-in duration-200"
-                    From: "opacity-100 translate-y-0 sm:scale-100"
-                    To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            -->
-            <div class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+            <!-- Modal panel, show/hide based on modal state. -->
+            <div
+                class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6"
+                x-show="open"
+                @click.outside="location.hash = '#'"
+                x-transition:enter="ease-out duration-300"
+                x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
+                x-transition:leave="ease-in duration-200"
+                x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
+                x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
                 <div class="absolute right-0 top-0 hidden pr-4 pt-4 sm:block">
                     <a href="#">
                         <div class="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
